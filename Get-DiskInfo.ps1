0eefdcc1a77c125d4cd8757a7ac59a6662b7e4d6
John Shelton <john.shelton@lucky13solutions.com>
2017-05-02 09:52:15 -0400

    Adjusted script output and added mandatory parameter

--- Get-DiskInfo.ps1
+++ Get-DiskInfo.ps1
@@ -1,32 +1,64 @@
 ﻿#=======================================================================================
 # Get-LockedOutUserDetails
 # Created on: 2017-01-11
-# Version 1.0
-# Last Updated: 
+# Version 1.1
+# Last Updated: 2017-05-02
 # Last Updated by: John Shelton | c: 260-410-1200 | e: john.shelton@lucky13solutions.com
 #
 # Purpose: This script pulls the volume information for a remote server using WMI and 
-#          returns the info to the user in a Grid View
+#          returns the info to the user in an HTML file and opens it.
 #
 # Notes: 
 # 
-# Change Log:
+# Change Log: Adjusted script to use a mandatory parameter and output to HTML.
 # 
 #
-#=======================================================================================
-##
+#====================================================================================
+#
+# Define Parameteres
+#
+param (
+    [Parameter(Mandatory = $true, ValueFromPipeline =$true, ValueFromPipelineByPropertyName =$true)]
+    [string]$ServerName = $(throw "-ServerName is required.")
+)
+#
+# Define HTML Output Variables
+#
+$ExecutionStamp = Get-Date -Format yyyyMMdd_hh-mm-ss
+$path = "c:\PSReports\"
+$FilenamePrepend = 'RPT_'
+$FullFilename = "Get-DiskInfo.ps1"
+$FileName = $FullFilename.Substring(0, $FullFilename.LastIndexOf('.'))
+$FileExt = '.html'
+$OutputHTMLFile = $path + $FilenamePrePend + '_' + $FileName + '_' + $ExecutionStamp + $FileExt
+#
+$PathExists = Test-Path $path
+IF($PathExists -eq $False)
+    {
+    New-Item -Path $path -ItemType  Directory
+    }
+#
+# Configure HTML Header
+#
+$HTMLHead = "<style>"
+$HTMLHead += "BODY{background-color:white;}"
+$HTMLHead += "TABLE{border-width: 1px;border-style: solid;border-color: black;border-collapse: collapse;}"
+$HTMLHead += "TH{border-width: 1px;padding: 0px;border-style: solid;border-color: black;background-color:royalblue}"
+$HTMLHead += "TD{border-width: 1px;padding: 0px;border-style: solid;border-color: black;background-color:gainsboro}"
+$HTMLHead += "</style>"
+#
 # Clear the screen
 #
 Clear-Host
 #
 #
 # Load required components to use a Popup Window to get user input using VB
-[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") 
-Add-Type -AssemblyName Microsoft.VisualBasic
+# [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") 
+# Add-Type -AssemblyName Microsoft.VisualBasic
 #
 # Get Server name from user
 #
-$ServerName = [Microsoft.VisualBasic.Interaction]::InputBox('Enter the name of the server that you would like to get the disk info on','Server Name', 'ServerName')
+# $ServerName = [Microsoft.VisualBasic.Interaction]::InputBox('Enter the name of the server that you would like to get the disk info on','Server Name', 'ServerName')
 #
 # Hash Table for Drive Type
 #
@@ -61,5 +93,5 @@ ForEach ($TempDiskInfo in $AllServerDiskInfo) {
     Else {$Temp | Add-Member -MemberType NoteProperty -Name "%Free" -Value 0}
     $ServerDiskDetailInfo += $Temp
     }
-$ServerDiskDetailInfo | Out-GridView
-[System.Windows.Forms.MessageBox]::Show('Please click OK to close the script','End Script')
+$ServerDiskDetailInfo | ConvertTo-HTML Date,Server,DriveLetter,VolumeName,DriveType,FileSystem,"Size(GB)","FreeSpace(GB)" -Title "Disk Info for $ServerName" -body "$HTMLHead<H2> Disk Info for $ServerName </H2> </P>" | Set-Content $OutputHTMLFile
+Invoke-Item $OutputHTMLFile
